86bc9168867a462bc6067ea7099ebca5ce1e45b3
Tetiana-Korobkina-88 <kor1988tanya@gmail.com>
2022-08-23 18:17:09 +0200

    completed the recipe page

--- src/pages/Recipe.jsx
+++ src/pages/Recipe.jsx
@@ -5,7 +5,7 @@ import { Preloader } from "../components/Preloader";
 
 function Recipe() {
   const { id } = useParams();
-  const [recipe, setRecipe] = useState([]);
+  const [recipe, setRecipe] = useState({});
   const navigate = useNavigate();
 
   useEffect(() => {
@@ -20,12 +20,35 @@ function Recipe() {
         <div className="recipe">
           <h1>{recipe.strMeal}</h1>
           <img src={recipe.strMealThumb} alt={recipe.strMeal} />
-          <h6>Category: {recipe.strCategory}</h6>
-          {recipe.strArea ? <h6>Area: {recipe.strArea}</h6> : null}
+          <h6 style={{ fontWeight: "bold" }}>category: {recipe.strCategory}</h6>
+          {recipe.strArea ? (
+            <h6 style={{ fontWeight: "bold" }}>area: {recipe.strArea}</h6>
+          ) : null}
           <p>{recipe.strInstructions}</p>
-          {recipe.strYoutub ? (
+          <table className="striped">
+            <thead>
+              <tr>
+                <th>Ingredient</th>
+                <th>Measure</th>
+              </tr>
+            </thead>
+            <tbody>
+              {Object.keys(recipe).map((key) => {
+                if (key.includes("Ingredient") && recipe[key]) {
+                  return (
+                    <tr key={key}>
+                      <td>{recipe[key]}</td>
+                      <td>{recipe[`strMeasure${key.slice(13)}`]}</td>
+                    </tr>
+                  );
+                }
+                return null;
+              })}
+            </tbody>
+          </table>
+          {recipe.strYoutube ? (
             <div className="row">
-              <h5>Video Recipe</h5>
+              <h5 style={{ margin: "2rem 0 2rem" }}>Video Recipe</h5>
               <iframe
                 title={id}
                 src={`https://www.youtube.com/embed/${recipe.strYoutube.slice(
@@ -37,7 +60,11 @@ function Recipe() {
           ) : null}
         </div>
       )}
-      <button className="btn" onClick={() => navigate(-1)}>
+      <button
+        style={{ margin: "2rem 0" }}
+        className="btn"
+        onClick={() => navigate(-1)}
+      >
         Go back
       </button>
     </>
