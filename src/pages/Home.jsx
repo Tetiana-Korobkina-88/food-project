44aed7e52c7b116f3d99dfee7d3b9910be5eaff4
Tetiana-Korobkina-88 <kor1988tanya@gmail.com>
2022-08-24 13:29:46 +0200

    created search button

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,18 +2,37 @@ import { useState, useEffect } from "react";
 import { getAllCategories } from "../api";
 import { Preloader } from "../components/Preloader";
 import { CategoryList } from "../components/CategoryList";
+import { Search } from "../components/Search";
 
 function Home() {
   const [catalog, setCatalog] = useState([]);
 
+  const [filteredCatalog, setFilteredCatalog] = useState([]);
+
+  const handleSearch = (str) => {
+    setFilteredCatalog(
+      catalog.filter((item) =>
+        item.strCategory.toLowerCase().includes(str.toLowerCase())
+      )
+    );
+  };
+
   useEffect(() => {
     getAllCategories().then((data) => {
       setCatalog(data.categories);
+      setFilteredCatalog(data.categories);
     });
   }, []);
 
   return (
-    <>{!catalog.length ? <Preloader /> : <CategoryList catalog={catalog} />} </>
+    <>
+      <Search cb={handleSearch} />{" "}
+      {!catalog.length ? (
+        <Preloader />
+      ) : (
+        <CategoryList catalog={filteredCatalog} />
+      )}{" "}
+    </>
   );
 }
 export { Home };
